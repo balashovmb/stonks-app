cff31e409456e533c873728d1e05617ecf99fbf6
Muxa <balashovmb@gmail.com>
2021-07-01 11:37:46 +0500

    Tune get/add cash

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,18 +3,20 @@ class Portfolio < ApplicationRecord
   has_many :deals
   has_many :trade_positions
 
-  def change_cash_volume(params)
+  validates :cash, numericality: { greater_than_or_equal_to: 0 }
+
+  def get_or_add_cash(params)
     summ = params[:summ].to_i
     summ_in_cent = summ * 100
     case params[:operation]
     when 'deposite'
       update(cash: self.cash += summ_in_cent)
-      "You added #{summ} $ to deposite"
+      { success: true, message: "You added #{summ} $ to deposite" }
     when 'widthdraw'
-      return "You don't have that much cash" if self.cash < summ_in_cent
+      return { success: false, message: "You don't have that much cash" } if self.cash < summ_in_cent
 
-      update(cash: self.cash -= summ_in_cent)
-      "You have withdrawn #{summ} $"
+      self.cash -= summ_in_cent
+      { success: true, message: "You have withdrawn #{summ} $" } if update(cash: cash)
     end
   end
 end
--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Portfolio, type: :model do
   describe '#change_cash_volume' do
     let(:portfolio) { create(:portfolio) }
     it 'adds cash' do
-      portfolio.change_cash_volume(operation: 'deposite', summ: 1000)
+      portfolio.get_or_add_cash(operation: 'deposite', summ: 1000)
       expect(portfolio.cash).to eq 1100000
     end
     it 'widthdraws cash' do
-      portfolio.change_cash_volume(operation: 'widthdraw', summ: 1000)
+      portfolio.get_or_add_cash(operation: 'widthdraw', summ: 1000)
       expect(portfolio.cash).to eq 900000
     end
   end
