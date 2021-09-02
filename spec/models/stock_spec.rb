4095af4c7d70a84b22764c951b627a756bad7ba2
balashovmb <balashovmb@gmail.com>
2021-09-02 10:57:21 +0500

    Fix creating unexisted stock

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -4,10 +4,19 @@ RSpec.describe Stock, type: :model do
   it { should validate_presence_of(:ticker) }
 
   describe '.get_and_create_stock' do
-    let(:stock_json) { File.read("#{Rails.root}/spec/data/stock.json") }
-    it 'creates stock' do
-      allow(Stock::FetchData).to receive(:call).and_return({ stock_json: stock_json, status: 200, source: 'tradier' })
-      expect { Stock.get_and_create_stock('AAPL') }.to change { Stock.count }.by(1)
+    context 'existed stock' do
+      let(:stock_json) { File.read("#{Rails.root}/spec/data/stock.json") }
+      it 'creates stock' do
+        allow(Stock::FetchData).to receive(:call).and_return({ stock_json: stock_json, status: 200, source: 'tradier' })
+        expect { Stock.get_and_create_stock('AAPL') }.to change { Stock.count }.by(1)
+      end
+    end
+    context 'unknown ticker' do
+      let(:stock_json) { '{"quotes":{"unmatched_symbols":{"symbol":"WRONGSYMBOL"}}}' }
+      it 'don\'t creates stock' do
+        allow(Stock::FetchData).to receive(:call).and_return({ stock_json: stock_json, status: 200, source: 'tradier' })
+        expect { Stock.get_and_create_stock('WRONGSYMBOL') }.not_to change { Stock.count }
+      end
     end
   end
 
