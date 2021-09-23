d6ba205f27a08274b6254421aeca834a6b81f330
balashovmb <balashovmb@gmail.com>
2021-09-23 16:20:23 +0500

    f2

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
 
   root to: "stocks#index"
 
+  # mount ActionCable.server => "/cable"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
