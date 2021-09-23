de832fe4e577c3b8129695effa34102338f50f1a
balashovmb <balashovmb@gmail.com>
2021-09-23 17:56:10 +0500

    f3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "deals"
+import('styles/application.css');
 
 Rails.start()
 Turbolinks.start()
