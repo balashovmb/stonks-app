bf112a8d3a2407e7f2f7d31fad4b16e83172eddd
balashovmb <balashovmb@gmail.com>
2022-05-23 14:58:44 +0500

    Вowngrade spring to 3.1.1 due to console startup issues

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ group :development do
   gem 'capistrano-sidekiq', require: false
   gem 'listen', '~> 3.3'
   gem 'rack-mini-profiler', '~> 2.0'
-  gem 'spring'
+  gem 'spring', '~> 3.1.1'
   gem 'web-console', '>= 4.1.0'
 end
 
--- docker-dev/docker-entrypoint.sh
+++ docker-dev/docker-entrypoint.sh
@@ -7,3 +7,12 @@ if [ -f tmp/pids/server.pid ]; then
 fi
 
 bundle exec rails s -b 0.0.0.0
+
+# if [ -z "$RAILS_ENV" ]; then
+#   echo "RAILS_ENV variable is not set, will use development by default"
+#   # bundle exec rails db:reset
+#   # bundle exec rails db:migrate
+#   bundle exec rspec
+# else
+#   bundle exec rails s -e $RAILS_ENV -p 3000 -b 0.0.0.0
+# fi
